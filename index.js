f756f60d28aec9637569e8b4c98984893954912b
erlin <erlin.hx@gmail.com>
2024-09-21 01:10:35 +0100

    .

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ fastify.register(Mercurius, {
 });
 
 // Start the Fastify server
-fastify.listen({ port: 3000 }, (err, addr) => {
+fastify.listen({ port: process.env.PORT || 3000 }, (err, addr) => {
   if (err) {
     console.error(err);
-    process.exit(1);
   }
   console.log(`Server running at http://localhost:3000`);
 });
